45be36c949076ae79fb9fe70fcda13b11f501b33
Salma Samir <salmashoair@gmail.com>
2020-01-21 01:20:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -25,19 +25,17 @@ function getLocation() {
 
 
 function returnLocation() {
-    // setTimeout(function () {
         navigator.geolocation.getCurrentPosition(position => {
             var lat, lng, mapLink, timeZone;
 
             lat = position.coords.latitude;
             lng = position.coords.longitude;
             mapLink = "https://www.google.com/maps/place/" + position.coords.latitude + "," + position.coords.longitude;
-            timeZone = new Date().getTimezoneOffset() / 60;
+            timeZone = new Date().getTimezoneOffset() / -60;
 
             document.getElementById("text1").value = "Latitude: " + lat;
             document.getElementById("text2").value = "Longitude: " + lng;
             document.getElementById("text3").value = "Link: " + mapLink;
             document.getElementById("text4").value = "Time Zone: " + timeZone;
         });
-//    }, 2);
 }
